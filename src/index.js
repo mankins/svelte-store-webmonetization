d718d5d005d682c0c9abc4ea1e7b73d478bf8106
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-02 02:42:08 +0530

    fix: return store in absence of document.monetization

--- src/index.js
+++ src/index.js
@@ -11,67 +11,7 @@ const { subscribe, update } = writable(webmonStoreInitState);
 
 const webmonStore = () => {
   if (typeof window !== "undefined") {
-    try {
-      // try to bail early if we don't have support
-      if (!document.monetization) {
-        update((store) => {
-          store.status = "missing-webmon";
-          store.monetized = false;
-          return store;
-        });
-        return; // no listeners setup
-      }
-
-      // check the current state right away
-      const { state } = document.monetization;
-      update((store) => {
-        store.status = "supports-webmon";
-        store.state = state;
-        return store;
-      });
-
-      // setup our start event listener
-      document.monetization.addEventListener("monetizationstart", (ev) => {
-        update((store) => {
-          store.monetized = true;
-          store.state = state;
-          return store;
-        });
-      });
-      document.monetization.addEventListener("monetizationstarted", (ev) => {
-        update((store) => {
-          store.monetized = true;
-          store.state = state;
-          return store;
-        });
-      });
-
-      // setup our progress event listener
-      document.monetization.addEventListener("monetizationprogress", (ev) => {
-        // debounce?
-        update((store) => {
-          store.progress = { ev, ts: Date.now() };
-          store.state = state;
-          if (state === "started") {
-            store.monetized = true;
-          } else {
-            store.monetized = false;
-          }
-          return store;
-        });
-      });
-
-      // check the initial state
-      if (state === "stopped") {
-        update((store) => {
-          store.monetized = false;
-          store.state = state;
-          return store;
-        });
-      }
-    } catch (e) {
-      console.log("webmon error", e);
-    }
+    init();
   }
 
   return {
@@ -79,5 +19,69 @@ const webmonStore = () => {
     // todo get events, subscribe to more fn
   };
 };
+
+function init() {
+  // try to bail early if we don't have support
+  if (!document.monetization) {
+    update((store) => {
+      store.status = "missing-webmon";
+      store.monetized = false;
+      return store;
+    });
+    return; // no listeners setup
+  }
+
+  try {
+    // check the current state right away
+    const { state } = document.monetization;
+    update((store) => {
+      store.status = "supports-webmon";
+      store.state = state;
+      return store;
+    });
+
+    // setup our start event listener
+    document.monetization.addEventListener("monetizationstart", (ev) => {
+      update((store) => {
+        store.monetized = true;
+        store.state = state;
+        return store;
+      });
+    });
+    document.monetization.addEventListener("monetizationstarted", (ev) => {
+      update((store) => {
+        store.monetized = true;
+        store.state = state;
+        return store;
+      });
+    });
+
+    // setup our progress event listener
+    document.monetization.addEventListener("monetizationprogress", (ev) => {
+      // debounce?
+      update((store) => {
+        store.progress = { ev, ts: Date.now() };
+        store.state = state;
+        if (state === "started") {
+          store.monetized = true;
+        } else {
+          store.monetized = false;
+        }
+        return store;
+      });
+    });
+
+    // check the initial state
+    if (state === "stopped") {
+      update((store) => {
+        store.monetized = false;
+        store.state = state;
+        return store;
+      });
+    }
+  } catch (e) {
+    console.error("webmon error", e);
+  }
+}
 
 export const webmon = webmonStore();
